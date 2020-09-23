9fecea9b0871a55faa9748959ca69b98e4f4eae3
Leonas PC <leonaspc@Leonass-iMac.local>
2020-09-23 12:03:12 +0800

    update

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,10 @@
-import React from "react"
-import Layout from "../components/layout"
+import React from "react";
+import Layout from "../components/layout";
 
 const Contact = () => (
 	<Layout>
 		<p>Contact Page</p>
 	</Layout>
-)
+);
 
-export default Contact
+export default Contact;
